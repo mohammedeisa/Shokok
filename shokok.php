45f7fc20c055bdcd8318f038af25516fe778d3ca
mohammedeisa <mohammed.ezz.2011@gmail.com>
2020-03-06 19:53:22 +0200

    First commit

---
+++ shokok.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Shokok
+Plugin URI: http://shokok.com
+description: Shokok handles the installments in the simplest way
+Version: 1.2
+Author: El ka7yaneen
+Author URI: http://shokok.com
+License: GPL2
+*/
